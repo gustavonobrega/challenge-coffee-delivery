392b77988fe078bc79595afa008383369d557375
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-08-17 15:55:39 -0300

    Create Success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,9 +1,51 @@
-import { SuccessContainer } from './styles'
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+import confirmedOrder from '../../assets/confirmed-order.png'
+import { CustomIcon } from '../../components/CustomIcon'
+
+import { SuccessContainer, ConfirmedOrder, OrderInfos } from './styles'
 
 export function Success() {
   return (
     <SuccessContainer>
-      <h1>Success</h1>
+      <ConfirmedOrder>
+        <h1>Uhu! Pedido confirmado</h1>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+
+        <div>
+          <OrderInfos>
+            <CustomIcon iconBg="map">
+              <MapPin size={16} weight="fill" />
+            </CustomIcon>
+
+            <p>
+              Entrega em <b>Rua João Daniel Martinelli, 102</b> <br />
+              Farrapos - Porto Alegre, RS
+            </p>
+          </OrderInfos>
+          <OrderInfos>
+            <CustomIcon iconBg="timer">
+              <Timer size={16} weight="fill" />
+            </CustomIcon>
+
+            <p>
+              Previsão de entrega <br />
+              <b>20 min - 30 min</b>
+            </p>
+          </OrderInfos>
+          <OrderInfos>
+            <CustomIcon iconBg="dollar">
+              <CurrencyDollar size={16} weight="fill" />
+            </CustomIcon>
+
+            <p>
+              Pagamento na entrega <br />
+              <b>Cartão de Crédito</b>
+            </p>
+          </OrderInfos>
+        </div>
+      </ConfirmedOrder>
+
+      <img src={confirmedOrder} alt="" />
     </SuccessContainer>
   )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,56 @@
 import styled from 'styled-components'
 
-export const SuccessContainer = styled.div``
+export const SuccessContainer = styled.main`
+  width: 100%;
+  margin-top: 5.25rem;
+
+  display: flex;
+  align-items: flex-end;
+  gap: 6.375rem;
+`
+export const ConfirmedOrder = styled.div`
+  min-width: 32.875rem;
+
+  h1 {
+    font-family: 'Baloo 2', sans-serif;
+    color: ${(props) => props.theme['yellow-dark']};
+    line-height: 1.3;
+  }
+
+  > p {
+    font-size: 1.25rem;
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+
+  > div {
+    margin-top: 2.5rem;
+    padding: 2.5rem;
+    background: ${(props) => props.theme.background};
+    position: relative;
+
+    border-radius: 6px 36px 6px 36px;
+
+    &::before {
+      content: '';
+      position: absolute;
+      inset: 0;
+      background: linear-gradient(102.89deg, #dbac2c 2.61%, #8047f8 98.76%);
+      margin: -1px;
+      z-index: -1;
+      border-radius: 7px 37px 7px 37px;
+    }
+  }
+`
+export const OrderInfos = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  & + div {
+    margin-top: 2rem;
+  }
+
+  p {
+    line-height: 1.3;
+  }
+`
