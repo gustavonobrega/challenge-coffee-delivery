b4f8b9ac25a79161d52da28675f038240b694eaa
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-08-26 16:23:35 -0300

    Implement CartContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { CartContextProvider } from './contexts/CartContext'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 
@@ -9,10 +10,12 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
-
-        <GlobalStyle />
+        <CartContextProvider>
+          <Router />
+        </CartContextProvider>
       </BrowserRouter>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,99 @@
+import { createContext, ReactNode, useState } from 'react'
+
+import { coffees } from '../data/coffees'
+
+interface CartContextProviderProps {
+  children: ReactNode
+}
+
+export interface CartCoffee {
+  id: number
+  photo: string
+  name: string
+  price: number
+  amount: number
+}
+
+interface UpdateCoffeeCart {
+  coffeeId: number
+  coffeeAmount: number
+}
+
+interface CartContextData {
+  cart: CartCoffee[]
+  addToCart: (coffeeId: number, quantity: number) => void
+  updateCoffeeCart: ({ coffeeId, coffeeAmount }: UpdateCoffeeCart) => void
+  removeCoffeeCart: (coffeeId: number) => void
+}
+
+export const CartContext = createContext({} as CartContextData)
+
+export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [cart, setCart] = useState<CartCoffee[]>([])
+
+  function addToCart(coffeeId: number, quantity: number) {
+    const cartCoffee = cart.find((coffee) => coffee.id === coffeeId)
+
+    if (!cartCoffee) {
+      const coffee = coffees.find((coffee) => coffee.id === coffeeId)
+
+      if (coffee) {
+        const newCart = {
+          id: coffee.id,
+          name: coffee.name,
+          photo: coffee.photo,
+          price: coffee.price,
+          amount: quantity,
+        }
+
+        setCart([...cart, newCart])
+      }
+    } else {
+      const updatedCart = cart.map((coffee) =>
+        coffee.id === coffeeId
+          ? {
+              ...coffee,
+              amount: coffee?.amount + quantity,
+            }
+          : coffee,
+      )
+
+      setCart(updatedCart)
+    }
+  }
+
+  function updateCoffeeCart({ coffeeId, coffeeAmount }: UpdateCoffeeCart) {
+    const cartCoffee = cart.find((coffee) => coffee.id === coffeeId)
+
+    if (cartCoffee) {
+      const newCart = cart.map((coffee) =>
+        coffee.id === coffeeId
+          ? {
+              ...coffee,
+              amount: coffeeAmount,
+            }
+          : coffee,
+      )
+
+      setCart(newCart)
+    }
+  }
+
+  function removeCoffeeCart(coffeeId: number) {
+    const cartCoffee = cart.find((coffee) => coffee.id === coffeeId)
+
+    if (cartCoffee) {
+      const newCart = cart.filter((coffee) => coffee.id !== coffeeId)
+
+      setCart(newCart)
+    }
+  }
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addToCart, removeCoffeeCart, updateCoffeeCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
